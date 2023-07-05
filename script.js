c40d5e04efc4117cc7606fa84b561777110b0908
xAndre12 <macarieandrei8@gmail.com>
2023-07-05 15:46:43 +0200

    a

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let character = document.querySelector(".characterRun");
+let terrain = document.querySelector(".terrain");
+let terrain2 = document.querySelector(".terrain2");
+let zombie = document.querySelector(".Zombie");
 
 let hasJump = false;
+terrain.style.left = 0;
+terrain2.style.left = "1920px";
+character.style.bottom = "170px";
+zombie.style.left = "1940px";
+
 const run = [
   "/img/ninja/run/Run__000.png",
   "/img/ninja/run/Run__001.png",
@@ -27,6 +35,19 @@ const jump = [
   "/img/ninja/jump/Jump__009.png",
 ];
 
+const zombieRun = [
+  "/img/zombie/walk/Walk1.png",
+  "/img/zombie/walk/Walk2.png",
+  "/img/zombie/walk/Walk3.png",
+  "/img/zombie/walk/Walk4.png",
+  "/img/zombie/walk/Walk5.png",
+  "/img/zombie/walk/Walk6.png",
+  "/img/zombie/walk/Walk7.png",
+  "/img/zombie/walk/Walk8.png",
+  "/img/zombie/walk/Walk9.png",
+  "/img/zombie/walk/Walk10.png",
+];
+
 setInterval(() => {
   if (!hasJump) {
     for (let index = 0; index < run.length; index++) {
@@ -34,21 +55,102 @@ setInterval(() => {
         character.src = run[index];
       }, 30 * index);
     }
+  }else{
+    for (let index = 0; index < jump.length; index++) {
+      setTimeout(() => {
+        character.src = jump[index];
+      }, 10 * index);
+    }
+  }
+  for (let index = 0; index < zombieRun.length; index++) {
+    setTimeout(() => {
+      zombie.src = zombieRun[index];
+    }, 30 * index);
   }
 }, 300);
 
-window.addEventListener("keypress", (event) => {
-  if (event.key === " ") {
-    hasJump = true;
+setInterval(() => {
+  if(parseInt(zombie.style.left) < -5){
+
+    zombie.style.left = "1940px"; 
+  }
+
+  for (let index = 0; index < 1940; index++) {
+    setTimeout(() =>{
+      zombie.style.left = parseInt(zombie.style.left)- 1 + "px";
+    },40 * index);
+  }
+}, 300);
+
+const groundMove = () => {
+  for (let index = 0; index < window.innerWidth * 2; index++) {
     setTimeout(()=>{
-        for (let index = 0; index < jump.length; index++) {
-            setTimeout(() => {
-              character.src = jump[index];
-            }, 20 * index);
-          }
-    }, 250)
+      terrain.style.left = parseInt(terrain.style.left) - 1 + "px";
+      if(parseInt(terrain.style.left) === -1920){
+        terrain.style.left = "1920px";
+        groundMove()
+      }
+    }, 4 * index)
+}}
+
+  for (let index = 0; index < window.innerWidth; index++) {
+ 
+    setTimeout(()=>{
+      terrain.style.left = parseInt(terrain.style.left) - 1 + "px";
+      if(parseInt(terrain.style.left) === -1920){
+        terrain.style.left = "1920px";
+        groundMove()
+      }
+    }, 4 * index)
+  }
+
+const groundMove2 = () => {
+  for (let index = 0; index < window.innerWidth * 2; index++) {
+    setTimeout(()=>{
+      terrain2.style.left = parseInt(terrain2.style.left) - 1 + "px";
+      if(parseInt(terrain2.style.left) === -1920){
+        terrain2.style.left = "1920px";
+        groundMove2()
+      }
+    }, 4 * index)
+}}
+
+  for (let index = 0; index < window.innerWidth * 2 ; index++) {
+ 
+    setTimeout(()=>{
+      terrain2.style.left = parseInt(terrain2.style.left) - 1 + "px";
+      if(parseInt(terrain2.style.left) === -1920){
+        terrain2.style.left = "1920px";
+        groundMove2()
+      }
+    }, 4 * index)
+  }
+
+
+
+
+window.addEventListener("keypress", (event) => {
+  if (event.key === " " &&  parseInt(character.style.bottom) === 170) {
+    hasJump = true;
+     
     setTimeout(() => {
         hasJump = false;
-    }, 700);
+    }, 250);
+
+    for (let index = 0; index < 300; index++) {
+      setTimeout(() => {
+        character.style.bottom =  parseInt(character.style.bottom) + 1 + "px";
+      }, 1 * index);
+    }
+
+    setTimeout(() => {
+      for (let index = 0; index < 300; index++) {
+        setTimeout(() => {
+          character.style.bottom =  parseInt(character.style.bottom) - 1 + "px";
+        }, 1 * index);
+      }
+    }, 240);
   }
 });
+
+tema: nor in spatele pamantului (sa fie mare)
